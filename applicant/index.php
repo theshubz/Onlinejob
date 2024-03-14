5ae23c792198eafec6fc809493bbe1db7c969caf
theshubz <157612314+theshubz@users.noreply.github.com>
2024-03-14 15:30:10 +0530

    update

--- applicant/index.php
+++ applicant/index.php
@@ -1,6 +1,7 @@
 <?php 
 require_once("../include/initialize.php");  
-if (isset($_SESSION['APPLICANTID'])) {
+if (!isset($_SESSION['APPLICANTID'])) {
+	# code...
 	redirect(web_root.'index.php');
 }
 $view = (isset($_GET['view']) && $_GET['view'] != '') ? $_GET['view'] : '';
@@ -8,26 +9,25 @@ switch ($view) {
 	case 'appliedjobs' :
 	    $title="Profile";	
         $_SESSION['appliedjobs']	='active' ; 
-		$content ='P\profile.php';
+		$content ='Profile.php';
 		break;
 
 	case 'notification' :
 	    $title="Profile";	
         $_SESSION['notification']	='active' ; 
-		$content ='profile.php';
+		$content ='Profile.php';
 		break;
   
 	case 'accounts' : 
 	    $title="Profile";	
         $_SESSION['accounts']	='active' ;
-        $content ='profile.php';
+        $content ='Profile.php';
 		break;
-	
 	 
 	default : 
 	    $title="Profile";	
         $_SESSION['appliedjobs']	='active' ;
-		$content ='profile.php';		
+		$content ='Profile.php';		
 }
 require_once("../theme/templates.php");
 ?>
